28446694772675a7872691607f303dba62d6534e
Ana Almeida <149433304+anaclaudia-almeida@users.noreply.github.com>
2023-11-28 16:37:39 -0300

    meu terceiro commit

---
+++ index.js
@@ -0,0 +1,24 @@
+
+class hero {
+  attack = "";
+
+  constructor (name, age, type) {
+    this.name = nome;
+    this.age = idade;
+    this.type = tipo;
+  }
+}
+
+  atacar();
+  
+    if (this.type ==="mago") {
+      this.attack = "magia";
+    } else if (this.type ==="guerreiro") {
+      this.attack = "espada";
+    } else if (this.type ==="monge") {
+      this.attack = "artes marciais";
+    } else if (this.type==="ninja") {
+      this.attack = "shuriken";
+    }
+    console.log (`O ${this.type} atacou usando ${this.attack}`)
+  
